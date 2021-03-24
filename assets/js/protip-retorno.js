f798f3808842ee9b1158af5d4ca66d0fe03e8007
Kevin <ikevin1992@gmail.com>
2021-03-24 00:33:41 -0500

    pro tip

--- assets/js/protip-retorno.js
+++ assets/js/protip-retorno.js
@@ -1,15 +1,31 @@
 
 
-function creatPersona ( nombre, apellido ) {
+// function crearPersona ( nombre, apellido ) {
+
+//     return {
+//         nombre,
+//         apellido
+//     };
+
+// }
+
+const crearPersona = ( nombre, apellido ) => ( { nombre, apellido } );
+
+const persona = crearPersona( 'Kevin', 'Echeverri' );
+console.log( persona );
 
 
-    return {
-        nombre,
-        apellido
-    };
-
+function imprimeArgumentos () {
+    console.log( arguments );
 }
 
+const imprimeArgumentos2 = ( edad, ...args ) => {
+    // console.log( { edad, args } );
+    return args;
+};
 
-const persona = creatPersona( 'Kevin', 'Echeverri' );
-console.log( persona );
+
+imprimeArgumentos( 'kevin', true, 10, false, 'Hola' );
+const argumentos = imprimeArgumentos2( 'kevin', true, 10, false, 'Hola' );
+
+console.log( { argumentos } );
