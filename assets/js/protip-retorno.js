e69ffe32f779373f43184ff963490fd0154a60ad
Kevin <ikevin1992@gmail.com>
2021-03-24 00:41:42 -0500

    pro tip funciones

--- assets/js/protip-retorno.js
+++ assets/js/protip-retorno.js
@@ -28,4 +28,39 @@ const imprimeArgumentos2 = ( edad, ...args ) => {
 imprimeArgumentos( 'kevin', true, 10, false, 'Hola' );
 const argumentos = imprimeArgumentos2( 'kevin', true, 10, false, 'Hola' );
 
+const [ casado, vivo, nombre, saludo ] = imprimeArgumentos2( 'kevin', true, 10, false, 'Hola' );
+
 console.log( { argumentos } );
+console.log( { casado, vivo, nombre, saludo } );
+
+
+const { apellido: nuevoApellido } = crearPersona( 'Kevin', 'Echeverri' );
+console.log( { nuevoApellido } );
+
+
+
+
+const tony = {
+    nombre: 'Tony Stark',
+    codeName: 'IronMan',
+    vivo: false,
+    edad: 40,
+    coords: {
+        lat: 45.2,
+        lon: -118.70
+    },
+    trajes: [ 'Mark I', 'Mark V', 'Hulkbuster' ],
+
+};
+
+
+const imprimePropiedades = ( { nombre, codeName, vivo, edad = 0, trajes } ) => {
+    console.log( { nombre } );
+    console.log( { codeName } );
+    console.log( { vivo } );
+    console.log( { edad } );
+    console.log( { trajes } );
+};
+
+
+imprimePropiedades( tony );
